8ae40a3382f5f42700c24b7d914e826ea4afd52b
Soo Philip Kim <philipjkim@gmail.com>
2013-03-27 21:45:18 +0900

    add routes for tasks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,17 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import play.api.mvc.Action
+import play.api.mvc.Controller
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect(routes.Application.tasks)
   }
-  
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 }
